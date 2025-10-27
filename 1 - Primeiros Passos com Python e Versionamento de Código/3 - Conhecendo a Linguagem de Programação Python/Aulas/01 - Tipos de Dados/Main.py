e2278509ce4a13f2833661c21a9d5ef032812c79
Paulo Ricardo Proenca <pauloricardoproe@gmail.com>
2025-10-26 22:41:42 -0300

    Reenviando arquivos

---
+++ 1 - Primeiros Passos com Python e Versionamento de Código/3 - Conhecendo a Linguagem de Programação Python/Aulas/01 - Tipos de Dados/Main.py
@@ -0,0 +1,30 @@
+def main():
+    # Tipos de números inteiros de dados:
+    idade = 21
+    print(idade)
+    print(10 + 10)
+    print(5.5 + 4)
+    print(5 - 10)
+    print(4 / 2)
+    print(5 * 5)
+    print(5 % 5)
+
+    # Tipos de números fluturantes de dados:
+    salario = 1500.00
+    print(round(salario, 2))
+    print(5.234 + 24.414)
+
+    # Tipos de caractéres:
+    variavelChar = "A" # char
+    nome = "Paulo Ricardo Proença" # string
+    print(variavelChar)
+    print(nome)
+
+    # Tipos de dados booleanos (verdadeiro ou falso):
+    maiorDeIdade = True
+    ricoSimOuNao = False
+    print(maiorDeIdade)
+    print(ricoSimOuNao)
+
+if __name__ == '__main__':
+    main()
